2a2a2006523d17919b13379951c8ee0bbab116af
MicroMidi <96044083+MicroMidi@users.noreply.github.com>
2022-06-08 09:41:05 +0200

    Update NUX-FS-MIDI-Client-OLED.ino
    Added an option to attach an analog effect pedal to adjust the master volume of the NUX device

--- src/NUX-FS-MIDI-Client-OLED.ino
+++ src/NUX-FS-MIDI-Client-OLED.ino
@@ -1,11 +1,13 @@
 /**
    --------------------------------------------------------
    This client code is based on the example file of author @RobertoHE incorporated in the BLE-MIDI library.
-   I adjusted it for the "Nux Mighty Plug" device and the Heltec ESP 32 WiFi Kit. This version supports four buttons:
+   I adjusted it for the "Nux Mighty Plug / Air" device and the Heltec ESP 32 WiFi Kit. This version supports four buttons:
    - two main push-buttons (2, 3) to decrement/increment the effect number
    - two addititional push-buttons (1, 4) to set the effect to the lowest/highest value.
    
-   The connection status, battery level and the current effect is shown on the integrated OLED display.
+   Optionally an analog volume pedal can be attached to the solution in order to controö the master volume of the NUX device
+
+   The connection status, battery level, the current effect and the master volume level is shown on the integrated OLED display.
 
    (authors: @RobertoHE/@MicroMidi)
    --------------------------------------------------------
@@ -13,7 +15,7 @@
 
 #include <Arduino.h>
 
-// Bluetooth MIDI
+// Bluetooth MIDI libraries
 #include <BLEMIDI_Transport.h>
 #include <hardware/BLEMIDI_Client_ESP32.h>
 //#include <hardware/BLEMIDI_ESP32_NimBLE.h>
@@ -27,29 +29,48 @@
 // Heltec WiFi Kit ESP32
 #include "heltec.h"
 
-// Power-Management
+// ESP32 Power-Management
 #include <esp_adc_cal.h>
 #include <driver/adc.h>
 
-// Connect to the NUX BLE-MIDI service
-BLEMIDI_CREATE_INSTANCE("NUX MIGHTY PLUG MIDI", MIDI)       //Connect to a specific name server
-//BLEMIDI_CREATE_DEFAULT_INSTANCE(); //Connect to first server found
-//BLEMIDI_CREATE_INSTANCE("",MIDI)                  //Connect to the first server found
-//BLEMIDI_CREATE_INSTANCE("f2:c1:d9:36:e7:6b",MIDI) //Connect to a specific BLE address server
+// USER DEFINITIONS START
+// ======================
+
+// Uncommenr matching device name for NUX Mighty Plug / Nux Mighty Air:
+// NUX Mighty Plug
+#define MIDI_DEVICE_NAME "NUX MIGHTY PLUG MIDI"
+// NUX Mighty Air
+// #define MIDI_DEVICE_NAME "NUX MIGHTY AIR MIDI"
+
+// Define PINs for all push buttons
+#define PIN_BUTTON_1 32
+#define PIN_BUTTON_2 33
+#define PIN_BUTTON_3 12
+#define PIN_BUTTON_4 13
+
+// Comment out PIN_VOLUME definition if NO volume pedal is attached to the solution
+#define PIN_VOLUME 36     // PIN for Volume Pedal
+
+// ======================
+// USER DEFINITIONS END
 
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 25 //modify for match with yout board
 #endif
 
-// PINs for all switches
-
-#define PIN_BUTTON_1 32
-#define PIN_BUTTON_2 33
-#define PIN_BUTTON_3 12
-#define PIN_BUTTON_4 13
-
 #define MAX_EFFECT_COUNT 7
+
+#ifdef PIN_VOLUME
+// Constants for volume pedal
+const int POT_THRESHOLD = 3;            // Threshold amount to guard against false values
+const int HISTORY_BUFFER_LENGTH = 10;    // History buffer length
+// (to guard against noise being sent)
+
+// Globals for volume pedal
+static int s_history[HISTORY_BUFFER_LENGTH];
+int MIDIVolume = -1;
+#endif
 
 Switch pushButton1 = Switch(PIN_BUTTON_1); // set effect number to lowest value
 Switch pushButton2 = Switch(PIN_BUTTON_2); // decrement effect
@@ -99,6 +120,11 @@ uint16_t Sample();
 void drawBattery(uint16_t, bool = false);
 esp_adc_cal_characteristics_t *adc_chars;
 
+// Connect to the NUX BLE-MIDI service
+BLEMIDI_CREATE_INSTANCE(MIDI_DEVICE_NAME, MIDI)       //Connect to a specific name server
+//BLEMIDI_CREATE_DEFAULT_INSTANCE(); //Connect to first server found
+//BLEMIDI_CREATE_INSTANCE("",MIDI)                  //Connect to the first server found
+//BLEMIDI_CREATE_INSTANCE("f2:c1:d9:36:e7:6b",MIDI) //Connect to a specific BLE address server
 
 /**
    -----------------------------------------------------------------------------
@@ -111,9 +137,22 @@ void setup()
   Serial.begin(115200);
   MIDI.begin(MIDI_CHANNEL_OMNI);
 
+#ifdef PIN_VOLUME
+
+  // Initialize ADC-input for volume pedal
+  adcAttachPin(PIN_VOLUME);
+  
+  // Initialize he buffer
+  for (int i = 0; i < HISTORY_BUFFER_LENGTH; i++)
+  {
+    s_history[i] = -1;
+  }
+#endif
+
   // Initalize display
   Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
-  Heltec.display->flipScreenVertically();
+  // Flip screen display is no longer needed with current Heltec board software and library version 1.1.1
+  // Heltec.display->flipScreenVertically();
   Heltec.display->setFont(ArialMT_Plain_10);
   Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
   Heltec.display->clear();
@@ -127,13 +166,6 @@ void setup()
   BLEMIDI.setHandleConnected([]()
   {
     Serial.println("---------CONNECTED---------");
-    // Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
-    //    Heltec.display->init();
-    //    Heltec.display->flipScreenVertically();
-    //    Heltec.display->clear();
-    //    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
-    //    Heltec.display->setFont(ArialMT_Plain_16);
-    //    Heltec.display->drawString(10, 20, "NUX connected");
     Heltec.display->drawXbm(0, 0, BT_width, BT_height, BT_bits);
     Heltec.display->display();
     isConnected = true;
@@ -146,15 +178,6 @@ void setup()
   BLEMIDI.setHandleDisconnected([]()
   {
     Serial.println("---------NOT CONNECTED---------");
-    // Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
-    // Heltec.display->init();
-    //    Heltec.display->flipScreenVertically();
-    //    Heltec.display->clear();
-    //    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
-    //    Heltec.display->setFont(ArialMT_Plain_16);
-    //    Heltec.display->drawString(50, 10, "NUX");
-    //    Heltec.display->drawString(25, 25, "disconnected");
-    //    Heltec.display->display();
     isConnected = false;
     FirstRun = true;
     CurrentEffect = 0;
@@ -174,6 +197,12 @@ void setup()
 
     CurrentEffect = ControlValue;
     DisplayEffect(CurrentEffect + 1);
+
+#ifdef PIN_VOLUME
+    // Adjust MIDI volume to pedal level
+    if (MIDIVolume >=0)
+      MIDI.sendControlChange(81, MIDIVolume, 1);
+#endif
   });
 
   xTaskCreatePinnedToCore(ReadCB,           //See FreeRTOS for more multitask info
@@ -220,6 +249,10 @@ void loop()
       Heltec.display->clear();
       DisplayEffect(CurrentEffect + 1);
       Heltec.display->display();
+
+#ifdef PIN_VOLUME
+      MIDIVolume = -1;
+#endif
     }
 
     pushButton1.poll();
@@ -229,6 +262,12 @@ void loop()
 
       MIDI.sendControlChange(49, CurrentEffect, 1);
       DisplayEffect(CurrentEffect + 1);
+
+#ifdef PIN_VOLUME
+      // Adjust MIDI volume to pedal level
+      if (MIDIVolume >= 0)
+        MIDI.sendControlChange(81, MIDIVolume, 1);
+#endif
     }
 
     pushButton2.poll();
@@ -241,6 +280,12 @@ void loop()
 
       MIDI.sendControlChange(49, CurrentEffect, 1);
       DisplayEffect(CurrentEffect + 1);
+
+#ifdef PIN_VOLUME
+      // Adjust MIDI volume to pedal level
+      if (MIDIVolume >= 0)
+        MIDI.sendControlChange(81, MIDIVolume, 1);
+#endif
     }
 
     pushButton3.poll();
@@ -253,6 +298,13 @@ void loop()
 
       MIDI.sendControlChange(49, CurrentEffect, 1);
       DisplayEffect(CurrentEffect + 1);
+
+#ifdef PIN_VOLUME
+      // Adjust MIDI volume to pedal level
+
+      if (MIDIVolume >= 0)
+        MIDI.sendControlChange(81, MIDIVolume, 1);
+#endif
     }
 
     pushButton4.poll();
@@ -262,7 +314,19 @@ void loop()
 
       MIDI.sendControlChange(49, CurrentEffect, 1);
       DisplayEffect(CurrentEffect + 1);
+
+#ifdef PIN_VOLUME
+      // Adjust MIDI volume to pedal level
+      if (MIDIVolume >= 0)
+        MIDI.sendControlChange(81, MIDIVolume, 1);
+#endif
     }
+
+#ifdef PIN_VOLUME
+    // Loop for reading volume pedal
+    ReadVolumePedal();
+#endif
+
   }
   // Loop for disconnected device
   else {
@@ -295,7 +359,6 @@ void loop()
         ScanCount++;
       else
         ScanCount = 0;
-
     }
   }
 
@@ -340,8 +403,52 @@ void DisplayEffect(int Effect)
   Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
   Heltec.display->setFont(ArialMT_Plain_24);
   Heltec.display->drawString(10, 20, "BANK: " + (String)(Effect));
+
+#ifdef PIN_VOLUME
+  if (MIDIVolume >= 0)
+    Heltec.display->drawProgressBar(0, 50, 120, 10, map(MIDIVolume, 0, 127, 0, 100));
+#endif
+
   Heltec.display->display();
 }
+
+#ifdef PIN_VOLUME
+void ReadVolumePedal()
+{
+  // Values of volume pedal
+  static int s_nLastPotValue = 0;
+  static int s_nLastMappedValue = 0;
+
+  adcStart(PIN_VOLUME);
+  while (adcBusy(PIN_VOLUME));
+  int nCurrentPotValue = analogRead(PIN_VOLUME);
+  adcEnd(PIN_VOLUME);
+
+  if (abs(nCurrentPotValue - s_nLastPotValue) < POT_THRESHOLD)
+    return;
+  s_nLastPotValue = nCurrentPotValue;
+
+  int nMappedValue = map(nCurrentPotValue, 0, 4095, 0, 127);    // Map the value to 0-127
+
+  if (nMappedValue == s_nLastMappedValue)
+    return;
+
+  for (int i = 0; i < HISTORY_BUFFER_LENGTH; i++)
+  {
+    if (s_history[i] == nMappedValue)
+      return;
+  }
+
+  memcpy(&s_history[0], &s_history[1], sizeof(int) * (HISTORY_BUFFER_LENGTH - 1));
+  s_history[HISTORY_BUFFER_LENGTH - 1] = nMappedValue;
+  s_nLastMappedValue = nMappedValue;
+  Serial.print("MIDI VolumeChange: ");
+  Serial.println(nMappedValue);
+  MIDI.sendControlChange(81, nMappedValue, 1);
+  MIDIVolume = nMappedValue;
+  DisplayEffect(CurrentEffect + 1);
+}
+#endif
 
 void InitPowerManagement()
 {
